1569a9cc122116dffe407de448701799136490ed
CerealYeti <geddypence@gmail.com>
2021-03-18 16:11:28 -0500

    added board, alliance, and more piece logic

--- project_files/src/com/chess/engine/board/Tile.java
+++ project_files/src/com/chess/engine/board/Tile.java
@@ -2,69 +2,71 @@ package com.chess.engine.board;
 
 import com.chess.engine.pieces.Piece;
 import com.google.common.collect.ImmutableMap;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Tile {
 
-    protected final int tileCoord;
-    private static final Map<Integer, EmtpyTile> EMPTY_TILES = createAllPossibleEmtpyTiles();
+  private static final Map<Integer, EmtpyTile> EMPTY_TILES_CACHE = createAllPossibleEmtpyTiles();
+  protected final int tileCoord;
 
-    private Tile(final int tileCoord) {
-        this.tileCoord = tileCoord;
+  private Tile(final int tileCoord) {
+    this.tileCoord = tileCoord;
+  }
+
+  private static Map<Integer, EmtpyTile> createAllPossibleEmtpyTiles() {
+    final Map<Integer, EmtpyTile> emtpyTileMap = new HashMap<>();
+
+    for (int i = 0; i < BoardUtils.NUM_TILES; i++) {
+      emtpyTileMap.put(i, new EmtpyTile(i));
     }
 
-    private static Map<Integer, EmtpyTile> createAllPossibleEmtpyTiles() {
-        final Map<Integer, EmtpyTile> emtpyTileMap = new HashMap<>();
+    return ImmutableMap.copyOf(emtpyTileMap);
+  }
 
-        for (int i = 0; i < 64; i++){
-            emtpyTileMap.put(i, new EmtpyTile(i));
-        }
+  public static Tile createTile(final int tileCoordinate, final Piece piece) {
+    return piece != null ? new OccupiedTile(tileCoordinate, piece)
+        : EMPTY_TILES_CACHE.get(tileCoordinate);
+  }
 
-        return ImmutableMap.copyOf(emtpyTileMap);
+  public abstract boolean isTileOccupied();
+
+  public abstract Piece getPiece();
+
+  public static final class EmtpyTile extends Tile {
+
+    private EmtpyTile(final int coordinate) {
+      super(coordinate);
     }
 
-    public static Tile createTile(final int tileCoordinate, final Piece piece){
-        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    @Override
+    public boolean isTileOccupied() {
+      return false;
     }
 
-    public abstract boolean isTileOccupied();
-    public abstract Piece getPiece();
+    @Override
+    public Piece getPiece() {
+      return null;
+    }
+  }
 
-    public static final class EmtpyTile extends Tile{
-        EmtpyTile(final int coordinate){
-            super(coordinate);
-        }
+  public static final class OccupiedTile extends Tile {
 
-        @Override
-        public boolean isTileOccupied(){
-            return false;
-        }
+    private final Piece pieceOnTile;
 
-        @Override
-        public Piece getPiece(){
-            return null;
-        }
+    private OccupiedTile(final int tileCoordinate, final Piece pieceOnTile) {
+      super(tileCoordinate);
+      this.pieceOnTile = pieceOnTile;
     }
 
-    public static final class OccupiedTile extends Tile {
-
-        private final Piece pieceOnTile;
-
-        OccupiedTile(final int tileCoordinate, final Piece pieceOnTile){
-            super(tileCoordinate);
-            this.pieceOnTile = pieceOnTile;
-        }
-
-        @Override
-        public boolean isTileOccupied() {
-            return true;
-        }
-
-        @Override
-        public Piece getPiece() {
-            return pieceOnTile;
-        }
+    @Override
+    public boolean isTileOccupied() {
+      return true;
     }
+
+    @Override
+    public Piece getPiece() {
+      return pieceOnTile;
+    }
+  }
 }
--- project_files/src/com/chess/engine/pieces/Piece.java
+++ project_files/src/com/chess/engine/pieces/Piece.java
@@ -1,5 +1,24 @@
 package com.chess.engine.pieces;
 
-public class Piece {
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import java.util.Collection;
+
+public abstract class Piece {
+
+  protected final int piecePosition;
+  protected final Alliance pieceAlliance;
+
+  Piece(int piecePosition, Alliance pieceAlliance) {
+    this.piecePosition = piecePosition;
+    this.pieceAlliance = pieceAlliance;
+  }
+
+  public abstract Collection<Move> calculateLegalMoves(final Board board);
+
+  protected Alliance getAlliance() {
+    return pieceAlliance;
+  }
 
 }
